9b5b4a2986160be0bbf0ac730af4303f05b24632
Yago César <yagocdj@gmail.com>
2024-08-11 13:05:33 -0300

    initial commit

---
+++ src/main/java/br/edu/ifpb/ecommercegenericbackend/ECommerceGenericBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.ecommercegenericbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceGenericBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceGenericBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpb/ecommercegenericbackend/ECommerceGenericBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.ecommercegenericbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceGenericBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
